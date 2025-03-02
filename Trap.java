f3ce4ee2ebdd2afd0a6e7e0ba4219360371e4cdc
JensonCrescini <144092734+JensonCrescini@users.noreply.github.com>
2025-03-02 22:45:39 +0800

    Update Trap.java

--- Trap.java
+++ Trap.java
@@ -1,14 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ccprog3_mco;
 
 /**
  *
  * @author cresc
  */
-public class Trap {
-    //trap display #
-    
+public class Trap extends Piece {
+
+    public Trap() {
+        super(0, 0, "Trap");
+    }
+
+    public String display() {
+        return "# ";
+    }
 }
